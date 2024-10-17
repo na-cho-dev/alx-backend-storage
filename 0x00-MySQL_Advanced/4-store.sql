7d6a23465853af803ae735d8ab8cfca33efc034e
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-17 23:12:08 +0100

    Task 4. Buy buy buy

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -6,8 +6,10 @@ DROP TRIGGER IF EXISTS decrease_qauntity;
 DELIMITER $$
 CREATE TRIGGER decrease_qauntity
 AFTER INSERT ON orders
-FOR EACH ROW BEGIN UPDATE items
-	SET quantity = quantity - NEW.number
+FOR EACH ROW
+BEGIN
+	UPDATE items
+	SET quantity := quantity - NEW.number
 	WHERE name = NEW.item_name;
 END $$
 DELIMITER ;
