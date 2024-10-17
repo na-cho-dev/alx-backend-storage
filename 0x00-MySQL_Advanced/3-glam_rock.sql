ef3d58722b420fd5a75b5de6ddfb7bed2f1f34b8
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-17 22:16:02 +0100

    Task 3. Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -8,5 +8,5 @@
 
 SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
+WHERE FIND_IN_SET('Glam rock', style)
 ORDER BY lifespan DESC;
