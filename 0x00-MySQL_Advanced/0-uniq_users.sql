7fc71220df76039c1f970ba32ef09c2053b0d57a
Fortune <nachodev369@gmail.com>
2024-10-17 12:47:07 +0000

    Task 0. We are all unique!

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -6,7 +6,7 @@
 -- If the table already exists, your script should not fail
 -- Your script can be executed on any database
 
-CREATE TABLE IF NOT EXISTSsers (
+CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
