ca7ed82d306aa48dae0be99ae9d72d44cc6a6da1
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-22 22:49:49 +0100

    Task 8. List all documents in Python

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,16 @@
-// List all documents in Python
+#!/usr/bin/env python3
+"""
+List all documents in Python
+"""
+
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a collection
+    Args:
+        mongo_collection: pymongo collection object
+    Returns:
+        All documents in a collection or an empty list if no document
+        in the collection
+    """
+    return [doc for doc in mongo_collection.find()]
