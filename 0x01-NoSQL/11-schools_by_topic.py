2b366760f99fdfb2273d708e0b10a12b1656331b
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-23 00:15:45 +0100

    Taask 11. Where can I learn Python?

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -5,4 +5,11 @@ Module: Where can I learn Python?
 
 
 def schools_by_topic(mongo_collection, topic):
-    pass
+    """Python function that returns a list of school
+    having a specific topic
+
+    Args:
+        mongo_collection: pymongo collection object
+        topic (string): Topic searched
+    """
+    return [doc for doc in mongo_collection.find({"topic": topic})]
