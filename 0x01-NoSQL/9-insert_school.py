98fe1d1cbead67b4814c367a26730ab6a48ea9d7
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-22 23:02:03 +0100

    Task 9. Insert a document in Python

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Insert a document in Python
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Python function that inserts a new document
+    in a collection based on kwargs
+
+    Args:
+        mongo_collection -> pymongo collection object
+        kwargs           -> Variable number of key vaue args
+    """
+    result = mongo_collection.insert_many(kwargs)
+
+    return result
