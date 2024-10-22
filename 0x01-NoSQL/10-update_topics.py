7c629b6559b75ffca6a9ff845660734f631d287a
Fortune Iheanacho <nachodev369@gmail.com>
2024-10-23 00:09:21 +0100

    Task 10. Change school topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+A python module: Change school topics
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Python function that changes all topics
+    of a school document based on the name:
+    """
+    mongo_collection.update({"name": name}, {"$set": {"topics": topics}})
